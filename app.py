6d688a00862a6b5bf34d311a8920c14db73af674
GodEmperorKing <youralltoeasy@gmail.com>
2026-04-29 14:47:02 -0700

    Update and rename app.py to greetings.py

--- app.py
+++
@@ -1,8 +0,0 @@
-print("Hello, World!")
-
-def greet():
-    return "Hello, class!"
-
-result = greet()
-
-print(result)
---
+++ greetings.py
@@ -0,0 +1,3 @@
+def generate_greeting(name):
+    """Returns a custom greeting string."""
+    return f"Access granted. Welcome to the Cloud Proving Ground, {name}!"
